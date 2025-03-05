bfef8408ff849bdc31f3046be30df86785291002
smira080 <smira040@ucr.edu>
2025-03-05 03:57:50 -0800

    updated files

---
+++ test/build/build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/csmajs/smira040/myRobot/lib/googletest/googlemock
+# Build directory: /home/csmajs/smira040/myRobot/test/build/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
